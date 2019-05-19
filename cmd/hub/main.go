bd4db627c4d1be3b68cc767a11cc1e2222e5d28c
Eyüp Ferhat GÜDÜCÜ <eferhatg@gmail.com>
2019-05-19 20:17:20 +0300

    Wrote hub details

--- cmd/hub/main.go
+++ cmd/hub/main.go
@@ -6,13 +6,10 @@ import (
 	"github.com/eferhatg/uinty-assignment/pkg/hub"
 )
 
-// only needed below for sample processing
-
 func main() {
 
 	fmt.Println("Launching server...")
 
 	h := hub.NewHub()
 	h.Listen(1087)
-
 }
