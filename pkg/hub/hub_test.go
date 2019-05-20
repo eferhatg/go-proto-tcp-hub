f38a48b186038c82861c8c80ceeeb9531461b14d
Eyüp Ferhat GÜDÜCÜ <eferhatg@gmail.com>
2019-05-20 20:00:30 +0300

    Wrote TestHub_MultiClientList

--- pkg/hub/hub_test.go
+++ pkg/hub/hub_test.go
@@ -2,16 +2,78 @@ package hub
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net"
 	"reflect"
+	"sort"
 	"strconv"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/eferhatg/uinty-assignment/pkg/protocol"
 	"github.com/golang/protobuf/proto"
 )
+
+const (
+	minTCPPort         = 0
+	maxTCPPort         = 65535
+	maxReservedTCPPort = 1024
+	maxRandTCPPort     = maxTCPPort - (maxReservedTCPPort + 1)
+)
+
+var (
+	tcpPortRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+)
+
+// IsTCPPortAvailable returns a flag indicating whether or not a TCP port is
+// available.
+func IsTCPPortAvailable(port int) bool {
+	if port < minTCPPort || port > maxTCPPort {
+		return false
+	}
+	conn, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", port))
+	if err != nil {
+		return false
+	}
+	conn.Close()
+	return true
+}
+
+// RandomTCPPort gets a free, random TCP port between 1025-65535. If no free
+// ports are available -1 is returned.
+func FindAvailablePort() int {
+	for i := maxReservedTCPPort; i < maxTCPPort; i++ {
+		p := tcpPortRand.Intn(maxRandTCPPort) + maxReservedTCPPort + 1
+		if IsTCPPortAvailable(p) {
+			return p
+		}
+	}
+	return -1
+}
+
+// func FindAvailablePort() int {
+
+// 	for {
+
+// 		rand.Seed(time.Now().UnixNano())
+// 		min := 10000
+// 		max := 99999
+// 		port := rand.Intn(max-min) + min
+// 		ln, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+
+// 		if err != nil {
+// 			continue
+// 		}
+
+// 		ln.Close()
+// 		time.Sleep(time.Second)
+// 		return port
+// 	}
+// }
 
 func TestHub_NewHub(t *testing.T) {
 	h := NewHub()
@@ -26,9 +88,11 @@ func TestHub_NewHub(t *testing.T) {
 }
 
 func TestHub_SimpleClientMsgSendRecieve(t *testing.T) {
+	port := FindAvailablePort()
 	h := NewHub()
-	go h.Start(8731)
-	conn, err := net.Dial("tcp", ":8731")
+	go h.Start(port)
+	time.Sleep(time.Second)
+	conn, err := net.Dial("tcp", ":"+strconv.Itoa(port))
 
 	if err != nil {
 		t.Error(err.Error())
@@ -59,9 +123,9 @@ func TestHub_SimpleClientMsgSendRecieve(t *testing.T) {
 
 }
 
-func TestHub_1000ClientIdentity(t *testing.T) {
+func TestHub_MultiClientIdentity(t *testing.T) {
 
-	clientCount := 1000
+	clientCount := 5
 	/*
 		Read handler function
 	*/
@@ -75,7 +139,7 @@ func TestHub_1000ClientIdentity(t *testing.T) {
 					t.Error("Failed")
 				}
 			} else {
-				log.Printf("Read %s bytes from client ", strconv.Itoa(nn))
+				log.Printf("Read %s bytes from IDENTITY RESPONSE. Client: ", strconv.Itoa(nn))
 			}
 		}
 	}
@@ -84,11 +148,14 @@ func TestHub_1000ClientIdentity(t *testing.T) {
 		Init New Hub
 		Connect clients
 	*/
+
+	port := FindAvailablePort()
 	h := NewHub()
-	go h.Start(4566)
+	go h.Start(port)
+	time.Sleep(time.Second)
 	conns := make([]*net.Conn, clientCount)
 	for i := 0; i < clientCount; i++ {
-		conn, err := net.Dial("tcp", ":4566")
+		conn, err := net.Dial("tcp", ":"+strconv.Itoa(port))
 		if err != nil {
 			t.Error("Failed to connect to tcp server on address ", err)
 			conn.Close()
@@ -116,6 +183,101 @@ func TestHub_1000ClientIdentity(t *testing.T) {
 	for i := 0; i < clientCount; i++ {
 		go handler(*conns[i])
 	}
+
+}
+
+func TestHub_MultiClientList(t *testing.T) {
+
+	clientCount := 5
+	/*
+		Read handler function
+	*/
+	handler := func(conn net.Conn) {
+		for {
+			out := make([]byte, 1024*1024)
+			_, err := conn.Read(out)
+			if err != nil {
+				conn.Close()
+				if err != io.EOF {
+					t.Error("Failed")
+				}
+			}
+		}
+	}
+
+	/*
+		Init New Hub
+		Connect clients
+	*/
+
+	port := FindAvailablePort()
+	h := NewHub()
+	go h.Start(port)
+	time.Sleep(time.Second)
+	conns := make([]*net.Conn, clientCount)
+	for i := 0; i < clientCount; i++ {
+		conn, err := net.Dial("tcp", ":"+strconv.Itoa(port))
+		if err != nil {
+			t.Error("Failed to connect to tcp server on address ", err)
+			conn.Close()
+
+		}
+		conns[i] = &conn
+	}
+
+	connSender, err := net.Dial("tcp", ":"+strconv.Itoa(port))
+	if err != nil {
+		t.Error("Failed to connect to tcp server on address ", err)
+		connSender.Close()
+
+	}
+
+	m := protocol.Message{
+		Command: protocol.Message_LIST,
+	}
+	bt, _ := proto.Marshal(&m)
+	if _, err := connSender.Write(bt); err != nil {
+		t.Error("Could not write to hub error:", err)
+	}
+
+	/*
+		Handle all connections read ops
+	*/
+	for i := 0; i < clientCount; i++ {
+		go handler(*conns[i])
+	}
+
+	var waitgroup sync.WaitGroup
+	waitgroup.Add(1)
+
+	/*Building sender connection*/
+	go func(conn net.Conn, waitgroup *sync.WaitGroup) {
+		for {
+			out := make([]byte, 1024*1024)
+			len, err := conn.Read(out)
+			if err != nil {
+				conn.Close()
+				if err != io.EOF {
+					t.Error("Failed")
+				}
+			} else {
+				m := &protocol.Message{}
+				err := proto.Unmarshal(out[:len], m)
+				if err != nil {
+					t.Error(err.Error())
+				}
+				cIds := m.GetConnectedClientIds()
+				sort.Slice(cIds, func(i, j int) bool { return cIds[i] < cIds[j] })
+
+				expected := []uint64{1, 2, 3, 4, 5}
+				if !reflect.DeepEqual(expected, cIds) {
+					t.Error("Wrong connected ids")
+				}
+				waitgroup.Done()
+			}
+		}
+	}(connSender, &waitgroup)
+	waitgroup.Wait()
 
 }
 
